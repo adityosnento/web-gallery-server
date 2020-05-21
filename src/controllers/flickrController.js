7a247b1de6d2a8d828e0a8f637f3f7ff1bd8e96f
Adityo Nento <adityosnento11@gmail.com>
2020-05-21 10:56:51 +0700

    [Adityo S. Nento] - Bug fix api_key

--- src/controllers/flickrController.js
+++ src/controllers/flickrController.js
@@ -4,19 +4,18 @@ const getPhoto = async (req, res) => {
   
   
   try {
-    const tag = req.query.tag
-    const text = req.query.text || ""
-    const page = req.query.page
+    const tag = req.query.tag || "home"
+    const text = req.query.text || "hello"
+    const page = req.query.page || 1
     const per_page = 12;
     const extras = "owner_name,tags,views,description,url_q,url_l";
-   
- 
-    const photos = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f5ff73b3b7aca7f78ffb4ae8c2a39ccb&page=${page}&per_page=${per_page}&extras=${extras}&text=${text}&tags=${tag}&format=json&nojsoncallback=1&safe_search=3&safe=3`)
+    
+    const photos = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=f5ff73b3b7aca7f78ffb4ae8c2a39ccb&page=${page}&per_page=${per_page}&extras=${extras}&text=${text}&tags=${tag}&format=json&nojsoncallback=1&safe_search=3&safe=3`)
+    console.log(tag,text,page,per_page)
     const data = photos.data.photos
-    console.log(data)
-        res.status(200).json({
-          success: true,
-          data: data
+    res.status(200).json({
+      success: true,
+      data: data
         })
   } catch (err) {
     res.status(422).json({
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const flickr = require('./controllers/flickrController.js')
 
-
 router.get('/flickr', flickr.getPhoto)
+
 
 module.exports = router;
